ef26f0b596152454b46db49053bdb8e87f886aa8
Dimas Putra <dimasputra@Locals-MacBook-Pro-3.local>
2026-08-18 14:49:48 +0700

    refactor base map

--- components/BaseMap.tsx
+++ components/BaseMap.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { useEffect } from "react";
-import { Map } from "maplibre-gl";
+import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function BaseMap() {
   useEffect(() => {
-    const map = new Map({
+    const map = new maplibregl.Map({
       container: "map",
       style:
         "https://v2.basemap.mapid.io/styles/street-v2.0/style.json?key=6a3255374eed59093aba9c32",
       center: [106.8271129, -6.1754398],
       zoom: 15.5,
-      pitch: 60,
+      pitch: 0,
       bearing: 0,
     });
 
